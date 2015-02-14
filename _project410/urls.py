31477c1ba71c54348188a61fb5566181ec608a82
btrinh1 <btrinh@ualberta.ca>
2015-02-14 00:39:18 -0700

    mainpage.html linked to default

--- _project410/urls.py
+++ _project410/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 import hello.views
+import polls.views
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', '_project410.views.home', name='home'),
