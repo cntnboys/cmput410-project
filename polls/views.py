bbc917818f689c38d03d95e74bda6840135034e6
btrinh1 <btrinh@ualberta.ca>
2015-02-14 00:48:14 -0700

    mainpage.html linked to default

--- polls/views.py
+++ polls/views.py
@@ -3,15 +3,9 @@ from django.shortcuts import render
 from polls.models import Authors, Friends, Posts, Comments, GithubStreams, TwitterStreams, FacebookStreams
 
 from django.http import HttpResponse
-from .models import Greeting
 # Create your views here.
 def index(request):
     return HttpResponse ("Hey, you're in polls.")
 
 def mainpage(request):
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, './templates/mainpage.html', {'greetings': greetings})
+    return render(request, './templates/mainpage.html')
