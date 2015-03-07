6e25f8c2a20a1aec2351e1316b5c8ef17a8e302b
Dan <djphan@gmail.com>
2015-03-06 19:45:42 -0700

    Adding UUID to model

--- author/models.py
+++ author/models.py
@@ -1,11 +1,14 @@
-from django.db import models
-from django.db import connection
 import datetime
-
 from django.utils import timezone
 
+
+from uuidfield import UUIDField
+
+from django.db import models
+from django.db import connection
+
 class Authors(models.Model):
-    author_id = models.AutoField(primary_key = True)
+    author_id = UUIDField(primary_key=True, auto=True, unique=True)
     name = models.CharField(max_length=200, null=False)
     username = models.CharField(max_length=30, null=False, unique=True)
     image = models.ImageField(upload_to="ProfileImages", max_length=250, null=True)
