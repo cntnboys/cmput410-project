0cd47ed570f86038d4e58cf30e1a53e3ec58c48e
Dan <djphan@gmail.com>
2015-03-09 20:49:15 -0600

    Fixed URL. Change URLs.

---
+++ main/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from main.models import Authors, Friends, Posts, Comments, GithubStreams, TwitterStreams, FacebookStreams
+
+# Register your models here.
+
+admin.site.register(Authors)
+admin.site.register(Friends)
+admin.site.register(Posts)
+admin.site.register(Comments)
+admin.site.register(GithubStreams)
+admin.site.register(TwitterStreams)
+admin.site.register(FacebookStreams)
