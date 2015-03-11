fbd8d492cea46a6b28b8f26dde813798d5f92d85
Dan <djphan@gmail.com>
2015-03-10 18:30:48 -0600

    Deleted Author Folder

--- author/Comment.py
+++
@@ -1,9 +0,0 @@
-
-class Comment(object):
-    """Creates Post object"""  
-    def __init__(self,author,commentcontent, pubdate):
-        self.author = author
-        self.content = commentcontent
-        self.pubdate = pubdate
-       
-      
